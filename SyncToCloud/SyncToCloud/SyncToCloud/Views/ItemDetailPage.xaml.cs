30d01e80f8358541e686836e6f2d221e5295c4be
sarbas33 <sarbas33@gmail.com>
2022-12-18 01:36:39 +0530

    First Commit

---
+++ SyncToCloud/SyncToCloud/SyncToCloud/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using SyncToCloud.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace SyncToCloud.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
